e355744a19e45ee19aa6b53caa86149014333359
Tobi Oriola <44350099+majivcorp@users.noreply.github.com>
2022-02-22 05:16:50 -0500

    Gitea push

---
+++ js-quest06/ex04/my_each.js
@@ -0,0 +1,21 @@
+// Commented out section was my process. - Just for future review and reflection
+//function my_each(param) {
+    
+    //index = 0;
+    //var len = param.length;
+    //var str = "";
+
+    //while (index < len) {
+        //return str = param[index];
+        //index++
+    ///}
+//}
+//console.log(my_each(["Banana", "Orange", "Apple", "Mango"]));
+
+function my_each(param) {
+    for (i = 0; i < param.length; i++) {
+        console.log(param[i]);
+    } 
+}
+
+//console.log(["blah1", "blah2", "blah3"]);
