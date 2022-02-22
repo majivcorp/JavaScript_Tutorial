585c6e25b6aa489569344e9f4c01fe31f102e9fa
Tobi Oriola <44350099+majivcorp@users.noreply.github.com>
2022-02-22 05:40:22 -0500

    Gitea Push
    #My Mastermind is an implementation of the famous game. 
    #SYNOPSIS 
    my_mastermind [-c]. 
    - My Mastermind is a game composed of 8 pieces of different colors. 
    - A secret code is then composed of 4 distinct pieces. 
    - The player has 10 attempts to find the secret code.

---
+++ my_mastermind/README.md
@@ -0,0 +1,4 @@
+My Mastermind is an implementation of the famous game. SYNOPSIS my_mastermind [-c]. 
+    - My Mastermind is a game composed of 8 pieces of different colors. 
+    - A secret code is then composed of 4 distinct pieces. 
+    - The player has 10 attempts to find the secret code.
---
+++ my_mastermind/my_mastermind/my_mastermind.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <time.h>
+#include <ctype.h>
+#define SEQ_LEN_CODE 4        //the length of the code
+#define NUMBER_COLOURS 8      //the number of colour options
+#define bool _Bool              //we define boolean type
+#define true 1                //we define true bool type
+#define false 0               //we define false bool type
+#define MAX_GUESSES 10        //max number of guesses  the user can attempt
+
